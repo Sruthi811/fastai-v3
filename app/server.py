a782f38fd8cfd58ea1ccb84e7060b4eaa35879d8
Sruthi811 <sruthi.ravichandiran@fau.de>
2020-01-30 18:45:24 +0100

    Update server.py

--- app/server.py
+++ app/server.py
@@ -8,8 +8,8 @@ from io import BytesIO
 from fastai import *
 from fastai.vision import *
 
-export_file_url = 'https://www.googleapis.com/drive/v3/files/1-HOTcS-EFk2y5_26hrdh392cCT1rWPKw?alt=media&key=AIzaSyD9Qkr9oc9uw9JFdy1D7pl2PuVRHH7D0wA'
-export_file_name = 'trained_model-save2.pkl'
+export_file_url = 'https://www.googleapis.com/drive/v3/files/101jtIMLhbdJRFDPqphT5A3fYp2EWTz9Q?alt=media&key=AIzaSyD9Qkr9oc9uw9JFdy1D7pl2PuVRHH7D0wA'
+export_file_name = 'trained-model-save-cy10-withtrain.pkl'
 
 classes = ['01_downdog', '02_chair', '03_boat', '04_triangle', '05_assorted_nontarget']
 path = Path(__file__).parent
